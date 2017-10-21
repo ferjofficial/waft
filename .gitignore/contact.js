e5a82bb47a6e6a3fc07763cc720caee7b532830a
Ferjohn Maglalang <ferjohnm@gmail.com>
2017-10-20 17:41:13 -0700

    Add files via upload

---
+++ .gitignore/contact.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    $('#email-form').validator();
+
+
+    // when the form is submitted
+    
+            return false;
+        }
+    })
+});
